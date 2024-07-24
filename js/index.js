cba91a7c1ccfd936d98c589805c8e4fddb42d9ca
aymen <jakebi@protonmail.com>
2024-07-24 23:13:21 +0100

    deleted:    LICENSE

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('scroll', function() {
     var element = document.getElementById('scroll-top');
     var scrollPosition = window.scrollY ;
 
-    if (scrollPosition < 700) { // Change this value to the scroll position you want
+    if (scrollPosition < 700) {
         element.style.visibility = "hidden";
         element.style.filter = "opacity(0)";
     } else {
@@ -12,4 +12,46 @@ document.addEventListener('scroll', function() {
     }
 });
 
+/*
+*/
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const root = document.documentElement;
+  
+    const lightModeVars = {
+      '--primary-color': '#fefdff',
+      '--secondary-color': '#2e52e1',
+      '--tertiary-color': '#1f2023', 
+    };
+  
+    const darkModeVars = {
+        '--primary-color': '#1f2023',
+        '--secondary-color': '#2e52e1',
+        '--tertiary-color': '#fefdff' 
+    };
+  
+    // Apply saved user preference
+    if (localStorage.getItem('theme') === 'dark') {
+      applyTheme(darkModeVars);
+    } else {
+      applyTheme(lightModeVars);
+    }
+  
+    darkModeToggle.addEventListener('click', () => {
+      if (localStorage.getItem('theme') === 'dark') {
+        applyTheme(lightModeVars);
+        localStorage.setItem('theme', 'light');
+      } else {
+        applyTheme(darkModeVars);
+        localStorage.setItem('theme', 'dark');
+      }
+    });
+  
+    function applyTheme(vars) {
+      for (const [key, value] of Object.entries(vars)) {
+        root.style.setProperty(key, value);
+      }
+    }
+  });
+  
 
